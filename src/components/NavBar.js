8bcdf01905c89f7c699b729b27cddcdfc82e7403
agustinDelpane <90353373+agustinDelpane@users.noreply.github.com>
2022-02-22 22:20:14 -0300

    Add files via upload

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+
+const NavBar = () => {
+  return (
+    <div>
+      <h1>Canton Games</h1>
+      <div class="container">
+      <ul class="nav">
+        <li><a href ="#"> Inicio </a></li>
+        <li><a href ="#"> PC </a></li>
+        <li><a href ="#"> Xbox SX </a></li>
+        <li><a href ="#"> Xbox One </a></li>
+        <li><a href ="#"> PS5 </a></li>
+        <li><a href ="#"> PS4 </a></li>
+        <li><a href ="#"> N Switch </a></li>
+        <li><a href ="#"> Ofertas </a></li>
+        <li><a href ="#"> Contacto </a></li>
+      </ul>
+    </div>
+    <div class="styleNav">
+    </div>   
+    </div>
+
+  )
+}
+
+export default NavBar
