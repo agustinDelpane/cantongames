ee9328258e59de3ae4450796d785c76229f7989a
agustinDelpane <90353373+agustinDelpane@users.noreply.github.com>
2022-03-03 16:44:52 -0300

    Add files via upload

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,45 @@
+import {useState} from "react";
+import './ItemCount.css'
+
+function ItemCount ({ initial, stock, onAdd }) {
+
+
+  const [count, setCount] = useState(initial)
+
+  const sumarCount = ()=> {
+      if (count < stock) {
+        setCount(count+1)
+      };
+  };
+
+  const restarCount = ()=> {
+    if (count > initial){
+      setCount (count-1)
+    }    
+
+  }
+
+  const restartCount = ()=> {
+    setCount(initial)
+  }
+
+  const agregarCar = ()=> {
+    console.log ("Se han agregado " + count + " productos")
+  }
+
+
+  return (
+
+    <div className="itemBox">
+      { ( ItemCount ) }
+       <label className="labelCount">{ count } </label>
+       <button onClick={ sumarCount } className="itemmas">  +  </button>
+       <button onClick={ restarCount } className="itemmenos">  -  </button>
+       <button onClick={ restartCount } className="itemRestart">  Reiniciar  </button>
+       <button onClick={ agregarCar } className="agregarCarr">Agregar al Carrito</button>
+    </div>
+
+  )
+};
+
+export default ItemCount
