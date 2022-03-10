7ded1af66e02588e6621244d49299ff30f6baa5e
agustinDelpane <90353373+agustinDelpane@users.noreply.github.com>
2022-03-10 16:51:28 -0300

    Add files via upload

---
+++ src/components/Item/ItemList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Item from './Item'
+
+function ItemList () {
+    return (
+        <Item></Item>
+    )
+}
+export default ItemList
---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import AccountComponent from './AccountWidget'
+import ImageComponent from './CartWidget'
+
+
+const NavBar = () => {
+  return (
+    <div>
+      <h1 className='nav'>Canton Games</h1>
+      <div className="container">
+      <ul className="nav">
+        <li><a href ="/"> Inicio </a></li>
+        <li><a href ="/"> PC </a></li>
+        <li><a href ="/"> Xbox SX </a></li>
+        <li><a href ="/"> Xbox One </a></li>
+        <li><a href ="/"> PS5 </a></li>
+        <li><a href ="/"> PS4 </a></li>
+        <li><a href ="/"> N Switch </a></li>
+        <li><a href ="/"> Ofertas </a></li>
+        <li><a href ="/"> Contacto </a></li>
+      </ul>
+    </div>
+    <ImageComponent />
+    <AccountComponent />
+    </div>
+  )
+}
+
+export default NavBar
