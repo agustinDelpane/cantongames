024d4cfe8d36d20b491c64d3ce0130976300cb1c
agustinDelpane <90353373+agustinDelpane@users.noreply.github.com>
2022-03-22 22:13:43 -0300

    Add files via upload

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,17 +2,15 @@ import React from 'react';
 import './../ItemListContainer.css'
 
 export const productos = [
-    { id: '1', nombre: 'DayZ', precio: '2000', img: <img src={require('./images/dayz.png')} className='dayzPng' alt='DayZ' /> },
-    { id: '2', nombre: 'Mass Effect Legendary Edition', precio: '3500', img: <img src={require('./images/me.png')} className='mePng' alt='Mass Effect LE' /> },
-    { id: '3', nombre: 'Star Wars Battlefront 2', precio: '800', img: <img src={require('./images/swb2.png')} className='swbPng' alt='Star Wars Battlefront 2' /> },
-    { id: '4', nombre: 'GTA V', precio: '500', img: <img src={require('./images/gtav.png')} className='gtavPng' alt='GTA V' /> },
-    { id: '5', nombre: 'Fall Guys', precio: '700', img: <img src={require('./images/fallguys.png')} className='fgPng' alt='Fall Guys' /> },
-    { id: '6', nombre: 'Dark Souls 3', precio: '1600', img: <img src={require('./images/ds3.png')} className='ds3Png' alt='Dark Souls 3' /> },
-    { id: '7', nombre: 'Resident Evil 8', precio: '3500', img: <img src={require('./images/re8.png')} className='re8Png' alt='Resident Evil 8' /> },
-    { id: '8', nombre: 'Age Of Empires 3', precio: '600', img: <img src={require('./images/aoe3.png')} className='aoe3Png' alt='Age Of Empires 3' /> }
+    { id: '1', nombre: 'DayZ', precio: '$2000', img: <img src={require('./images/dayz.png')} className='dayzPng' alt='DayZ' /> },
+    { id: '2', nombre: 'Mass Effect Legendary Edition', precio: '$3500', img: <img src={require('./images/me.png')} className='mePng' alt='Mass Effect LE' /> },
+    { id: '3', nombre: 'Star Wars Battlefront 2', precio: '$800', img: <img src={require('./images/swb2.png')} className='swbPng' alt='Star Wars Battlefront 2' /> },
+    { id: '4', nombre: 'GTA V', precio: '$500', img: <img src={require('./images/gtav.png')} className='gtavPng' alt='GTA V' /> },
+    { id: '5', nombre: 'Fall Guys', precio: '$700', img: <img src={require('./images/fallguys.png')} className='fgPng' alt='Fall Guys' /> },
+    { id: '6', nombre: 'Dark Souls 3', precio: '$1600', img: <img src={require('./images/ds3.png')} className='ds3Png' alt='Dark Souls 3' /> },
+    { id: '7', nombre: 'Resident Evil 8', precio: '$3500', img: <img src={require('./images/re8.png')} className='re8Png' alt='Resident Evil 8' /> },
+    { id: '8', nombre: 'Age Of Empires 3', precio: '$600', img: <img src={require('./images/aoe3.png')} className='aoe3Png' alt='Age Of Empires 3' /> }
 ]
 
-export default {
-    productos,
-}
 
+// { id: '1', nombre: 'DayZ', precio: '$2000', img: <img src={require('./images/dayz.png')} className='dayzPng' alt='DayZ' /> },
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import TituloGreet from './Greet/Titulo'
 import './input/input'
-import productos from './Item/Item.js'
+import { productos } from './Item/Item.js'
 import ItemCount from './ItemCount/ItemCount.js'
 import ItemDetailContainer from './Item/ItemDetailContainer'
+import { Card, Button }  from 'react-bootstrap'
 
 const titulo='Bienvenido a Canton Games!';
 
@@ -26,21 +27,35 @@ task.then(
   (error) => console.log(error)
 )
 
-
-
+// <ItemCount initial={ 1 } stock={ 10 } onAdd={ onAdd } />
+// <ItemDetailContainer />
+// <ul className='mapProd'> {prod.img } <br /> {prod.nombre} </ul>)
+// <Card.Img variant="top" src= { prod.img}  alt='Imagen del juego' />
 
 function ItemListContainer() {
-  return (
-
-    
+  return ( 
+    <>
     <div className='ILC'>
     <TituloGreet titulo={ titulo } saludo={() => console.log('Saludos, internauta!')  } />
+    </div>
+
     <div className='mapProd'>
-  { productos.map(prod => <ul className='mapProd'> {prod.img } <br /> {prod.nombre} </ul>)   }
+  { productos.map(prod => <Card style={{ width: '18rem' }}>
+
+  <Card.Body>
+    <Card.Title> { prod.nombre } </Card.Title>
+    <div className='imgCard'>
+      <img src={ prod.img } alt='Imagen del juego'></img>
     </div>
-    <ItemCount initial={ 1 } stock={ 10 } onAdd={ onAdd } />
-    <ItemDetailContainer />
-    </div>
+    <Card.Text>
+      { prod.precio }
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+  </Card>)
+  }
+</div>
+</>
   )
 }
 
